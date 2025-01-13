c02e06a4635bd31cdb1da20b26f703b4fdb59464
Aegis Local <aegis@local>
2025-01-13 14:22:42 +0100

    captors working

--- code/standby/watcher.py
+++ code/standby/watcher.py
@@ -8,7 +8,8 @@ try:
     import RPi.GPIO as GPIO
 except ImportError:
     from mockGPIO import GPIO
-    
+import Adafruit_DHT
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,13 +19,28 @@ class ControllerManager:
 
     #passive infrared
     PIR_PIN = int(os.getenv('PIR_PIN'))
+    CLAP_PIN = int(os.getenv('CLAP_PIN'))
+    TEMP_PIN = int(os.getenv('TEMP_PIN'))
+
+
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.PIR_PIN, GPIO.IN)	
+        GPIO.setup(self.PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#        GPIO.setup(self.CLAP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
     def get_pir(self):
-        return GPIO.input(self.PIR_PIN)
+        return bool(GPIO.input(self.PIR_PIN))
+
+    def get_clap(self):
+        return not(bool(GPIO.input(self.CLAP_PIN)))
+
+    def get_temp_humid(self):
+        start_time = time.time()
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, self.TEMP_PIN)
+        end_time = time.time()
+        rich.print(start_time, end_time, end_time - start_time)
+        return (humidity, temperature)
 
     # def update_buttons(self):
     #     for num in range(self.SIZE):
@@ -34,7 +50,7 @@ class ControllerManager:
 
     # def button_just_pressed(self, num):
     #     return not self.prev_button_state[num] and self.button_state[num]
-    
+
     # def button_just_released(self, num):
     #     return not self.prev_button_state[num] and self.button_state[num]
     # def get_buttons(self):
@@ -75,10 +91,23 @@ if __name__ == '__main__':
 
 #     contr.write_text("abcdefghi", 2)
 
+    oldval = False
+    oldcnt = 0
+
     while (True):
-        res = contr.get_pir()
-        rich.print(res)
-        
+        #res = contr.get_pir()
+#        res = contr.get_clap()
+#        if (oldval == res):
+#            oldcnt += 1
+#        else:
+#            oldcnt = 0
+
+#        oldval = res
+#        rich.print(res, oldcnt)
+
+        h, t = contr.get_temp_humid()
+        print(f"Measured Temp={t}°C | Hum={h}%")
+
         time.sleep(0.1)
 
 
