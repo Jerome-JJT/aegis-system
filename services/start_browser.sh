5a3f13169da45cc5f755bdd06d8d77e4106abb0f
AEGIS local <aegis@mail.com>
2025-02-19 17:58:32 +0100

    changes

--- services/start_browser.sh
+++ services/start_browser.sh
@@ -2,14 +2,15 @@
 
 export XDG_RUNTIME_DIR="/run/user/1000"
 
-/usr/bin/sleep 180
+/usr/bin/sleep 60
 
 echo "GIRST"
 echo $(/usr/bin/whoami)
 echo "SEC"
 
-(/usr/bin/sleep 260 && /usr/bin/sudo /home/admin/aegis-system/env/bin/python -c 'import os; import keyboard; import time; print(os.environ); keyboard.press_and_release("space"); time.sleep(2); keyboard.press_and_release("f11")' && echo 'KEYBOARD DONE' && >&2 echo "KEYBOARD PLOP") &
+#(/usr/bin/sleep 60 && /usr/bin/sudo /home/admin/aegis-system/env/bin/python -c 'import os; import keyboard; import time; print(os.environ); keyboard.press_and_release("space"); time.sleep(2); keyboard.press_and_release("f11")' && echo 'KEYBOARD DONE' && >&2 echo "KEYBOARD PLOP") &
 #/usr/bin/sudo /home/admin/aegis-system/env/bin/python -c 'import keyboard; import time; keyboard.press_and_release("space"); time.sleep(2); keyboard.press_and_release("f11")' &
 
 #/usr/bin/midori /home/admin/index.html
-/usr/bin/midori http://localhost:8000
+#/usr/bin/midori http://localhost:8000
+/usr/bin/epiphany http://localhost:8000
