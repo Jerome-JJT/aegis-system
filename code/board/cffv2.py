d895d9746d60d706e7a09137265d0e405fd0e117
Jerom2 <jerome.jaquemet@gmail.com>
2025-07-13 10:56:21 +0200

    v2

---
+++ code/board/cffv2.py
@@ -0,0 +1,189 @@
+import sys
+import time
+import math
+import rich
+import requests
+import datetime
+import pytz
+
+from .._common.utils import pluralize, cardinalinator, discord_time
+from .._common.discord_message import create_discord_payload, send_discord_payload
+
+def parse_loc(loc):
+
+    nloc = { 
+        "id": loc["stopid"],
+        "name": loc["name"],
+        "coordinate": {
+            "lat": loc["lat"],
+            "lon": loc["lon"],
+        },
+        
+        "time": loc["departure"] if 'departure' in loc.keys() else loc["arrival"],
+        "prono_time": loc["departure"] if 'departure' in loc.keys() else loc["arrival"],
+        # "prono_time": loc["prognosis"]["departure"] if loc["departure"] != None else loc["prognosis"]["arrival"],
+
+        "platform": loc["*L"] if 'departure' in loc.keys() else None,
+        # "platform": loc["platform"],
+        "prono_platform": loc["*L"] if 'departure' in loc.keys() else None,
+        # "prono_platform": loc["prognosis"]["platform"],
+
+        "delay": int(loc['dep_delay'][1:]) if 'departure' in loc.keys() else loc["normal_time"],
+        # "prognosis": loc["prognosis"],
+    }
+    
+    return nloc
+
+def parse_con(con):
+    
+    ncon = {
+        "start": parse_loc(con["legs"][0]),
+        "end": parse_loc(con["legs"][-1]),
+        "to": con["to"],
+        
+        "duration": con["duration"],
+        # "transfers": con["transfers"],
+        # "products": ",".join(con["products"]) if type(con["products"]) == type([]) else con["products"],
+        "category": con["legs"][0]["type"],
+        "name": con["legs"][0]["name"]
+    }
+
+    diff_first = {
+        "x": con["legs"][0]["lon"] - con["legs"][-1]["lon"],
+        "y": con["legs"][0]["lat"] - con["legs"][-1]["lat"]
+    }
+    diff_first = {
+        "x": con["legs"][0]["lon"] - con["legs"][-1]["lon"],
+        "y": con["legs"][0]["lat"] - con["legs"][-1]["lat"]
+    }
+    
+    ncon["raw_dir"] = math.atan2(diff_first["y"], diff_first["x"])
+    ncon["dir"] = cardinalinator(ncon["raw_dir"])
+
+    return ncon
+
+def conn_search(q="from=Chavornay&to=Renens", l=10):
+    l = 3
+    
+    # limiter = "&fields[]=connections/from&fields[]=connections/to&fields[]=connections/duration&fields[]=connections/transfers&fields[]=connections/products"
+
+    try:
+        # url = f"http://transport.opendata.ch/v1/connections?{q}&limit={l}"
+        url = f"https://search.ch/timetable/api/route.json?show_delays=1&show_trackchanges=1&{q}&num={l}"
+        rich.print("REQ", url)
+        req = requests.get(url)
+
+        if (req.ok):
+            js = req.json()
+
+            # rich.print(js)
+            cons = list(map(lambda x: parse_con({**x}), js["connections"]))
+
+            # rich.print(cons)
+            
+            return cons
+
+        else:
+            rich.print(f"[red]FAIL {req.status_code} {req.reason}")
+            return []
+
+    except requests.exceptions.ConnectionError:
+        rich.print(f"[red]FAIL ConnectionError")
+        return []
+    
+
+buffer = {}
+
+if (len(sys.argv) >= 2):
+    
+    if (sys.argv[1] == "0"):
+        aa = conn_search(l = 4)
+    
+    elif (sys.argv[1] == "2"):
+        pass
+        # while True:
+            
+            # # res = conn_search(q="from=Zurich&to=Munchen", l = 4)
+            # res = conn_search(l = 4)
+            # rich.print(res)
+            
+            # todel = list(buffer.keys())
+            
+            # for r in res:
+            #     changes = {}
+            #     # rich.print(r)
+            #     idd = r["name"]
+            #     if idd in list(buffer.keys()):
+            #         todel.remove(idd)
+                
+            #     if idd not in buffer.keys():
+            #         buffer[idd] = r
+                    
+            #         if (r["start"]["delay"] != None and r["start"]["delay"] >= 2):
+            #             changes["delay"] = {"old": "-", "new": r["start"]["delay"]}
+
+            #             if (r["start"]["prono_time"] != None and r["start"]["time"] != r["start"]["prono_time"]):
+            #                 changes["time"] = {
+            #                     "old": discord_time(r["start"]["time"]),
+            #                     "new": discord_time(r["start"]["prono_time"])
+            #                 }
+                        
+            #         if (r["start"]["prono_platform"] != None and r["start"]["platform"] != r["start"]["prono_time"]):
+            #             changes["platform"] = {
+            #                 "old": discord_time(r["start"]["platform"]),
+            #                 "new": discord_time(r["start"]["prono_platform"]),
+            #             }
+                        
+            #     cmpp = buffer[idd]
+
+            #     if (r["start"]["delay"] != cmpp["start"]["delay"]):
+            #         changes["delay"] = {
+            #             "old": cmpp["start"]["delay"], 
+            #             "new": r["start"]["delay"]
+            #         }
+                    
+            #     if (r["start"]["prono_time"] != cmpp["start"]["prono_time"]):
+            #         changes["prono_time"] = {
+            #             "old": discord_time(cmpp["start"]["prono_time"]),
+            #             "new": discord_time(r["start"]["prono_time"])
+            #         }
+                    
+            #     if (r["start"]["prono_platform"] != buffer[idd]["start"]["prono_platform"]):
+            #         changes["prono_platform"] = {
+            #             "old": discord_time(cmpp["start"]["prono_platform"]),
+            #             "new": discord_time(r["start"]["prono_platform"])
+            #         }
+                    
+            #     # rich.print(idd, changes)
+                
+            #     if (len(changes)):
+            #         embed = {
+            #             'message_type': 'embed',
+            #             # embed["thumbnail"] = f'{fetched["avatar_url"]}',
+            #             'footer_text': datetime.datetime.now().astimezone(tz=pytz.timezone('Europe/Zurich')).strftime('%Y-%m-%d %H:%M:%S')
+            #         }
+                    
+            #         # cat = "undefined"
+            #         # if (cmpp["category"] == "R"):
+            #         #     cat = "train"
+
+
+            #         embed['title'] = f'{r["start"]["name"]} to {r["to"]} at {cmpp['start']['time'][11:16]}\n'
+            #         embed['title'] += f'{cmpp['start']['delay']} {pluralize(cmpp['start']['delay'], 'minute', 'minutes')} delay'
+                    
+            #         embed['fields'] = dict({
+            #             k.capitalize(): f'ref: `{v["old"]}`{'\n' if len(str(v['new'])) > 10 else ' '}new: `{v["new"]}`' for k, v in changes.items()
+            #         })
+                    
+            #         payload = create_discord_payload(embed)
+            #         send_discord_payload(payload)
+
+            #         rich.print(idd, embed)
+            #         rich.print()
+                    
+                
+            # # rich.print("BUFFER", buffer)
+            # for to in todel:
+            #     del buffer[to]
+            
+            # time.sleep(60)
---
+++ code/board/serverv2.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+import rich
+import json
+import time
+
+import click
+import threading
+import datetime
+import pytz
+import websockets
+
+from websockets.sync.server import serve
+
+from code.board.cffv2 import conn_search
+from code._common.app_config import ConfigManager
+from code._common.discord_message import create_discord_payload, send_discord_payload
+from code._common.utils import pluralize, discord_time
+
+CLIENTS = dict()
+conf_manager = ConfigManager()
+buffers = {}
+
+
+def notify(changes, infos, conf_name):
+    embed = {
+        'message_type': 'embed',
+        # embed["thumbnail"] = f'{fetched["avatar_url"]}',
+        'footer_text': datetime.datetime.now().astimezone(tz=pytz.timezone('Europe/Zurich')).strftime('%Y-%m-%d %H:%M:%S')
+    }
+    
+    # cat = "undefined"
+    # if (cmpp["category"] == "R"):
+    #     cat = "train"
+
+
+    embed['title'] = f'{conf_name}: `{infos["start"]["name"]}` to `{infos["to"]}` at {infos["start"]["time"][11:16]}\n'
+    embed['title'] += f'{infos["start"]["delay"]} {pluralize(infos["start"]["delay"], "minute", "minutes")} delay'
+    
+    embed['fields'] = dict({
+        k.capitalize(): f'ref: {v["old"]}{chr(10) if len(str(v["new"])) > 0 else " "}new: {v["new"]}' for k, v in changes.items()
+    })
+    
+    payload = create_discord_payload(embed)
+    send_discord_payload(payload)
+
+
+
+def check_changes(elem, conf):
+    changes = {}
+    conf_id = conf.get("id")
+    elem_id = elem["name"]
+
+    # First changes detection
+    if elem_id not in buffers[conf_id].keys():
+        buffers[conf_id][elem_id] = {
+            **elem,
+            "last_update": round(datetime.datetime.timestamp(datetime.datetime.now()))
+        }
+        
+        if (elem["start"]["delay"] != None and elem["start"]["delay"] >= conf.get("min_delay") or 0):
+            changes["delay"] = {"old": "-", "new": elem["start"]["delay"]}
+
+            if (elem["start"]["prono_time"] != None and elem["start"]["time"] != elem["start"]["prono_time"]):
+                changes["time"] = {
+                    "old": discord_time(elem["start"]["time"]),
+                    "new": discord_time(elem["start"]["prono_time"])
+                }
+            
+        if (elem["start"]["prono_platform"] != None and elem["start"]["platform"] != elem["start"]["prono_time"]):
+            changes["platform"] = {
+                "old": discord_time(elem["start"]["platform"]),
+                "new": discord_time(elem["start"]["prono_platform"])
+            }
+
+    # Get old or actual if not exists
+    buffer_elem = buffers[conf_id][elem_id]
+
+    # Diff checks
+    if (elem["start"]["delay"] != None and 
+        (
+            (elem["start"]["delay"] > buffer_elem["start"]["delay"] and elem["start"]["delay"] >= conf.get("min_delay")) or
+            (elem["start"]["delay"] < buffer_elem["start"]["delay"] and buffer_elem["start"]["delay"] >= conf.get("min_delay")) 
+        )
+    ):
+        changes["delay"] = {
+            "old": buffer_elem["start"]["delay"], 
+            "new": elem["start"]["delay"]
+        }
+        
+        if (elem["start"]["prono_time"] != None and 
+            elem["start"]["prono_time"] != buffer_elem["start"]["prono_time"]
+        ):
+            changes["prono_time"] = {
+                "old": discord_time(buffer_elem["start"]["prono_time"]),
+                "new": discord_time(elem["start"]["prono_time"])
+            }
+        
+    if (elem["start"]["prono_platform"] != None and
+        elem["start"]["prono_platform"] != buffer_elem["start"]["prono_platform"]
+    ):
+        changes["prono_platform"] = {
+            "old": discord_time(buffer_elem["start"]["prono_platform"]),
+            "new": discord_time(elem["start"]["prono_platform"])
+        }
+
+    # time_compare = datetime.datetime.isoformat(parser.parse(elem["start"]["time"]))[11:]
+    time_compare = elem["start"]["time"][11:]
+
+    # rich.print(conf)
+    if (len(changes) > 0 and conf.get("notify_start") != None and (
+        conf.get("notify_end") == None or (
+            time_compare >= conf.get("notify_start") and 
+            time_compare <= conf.get("notify_end")
+        )
+    )):
+        notify(changes, elem, conf.get("name") or "")
+
+    buffers[conf_id][elem_id]["last_update"] = round(datetime.datetime.timestamp(datetime.datetime.now()))
+
+def update(check):
+    conf_id = check["id"]
+    if (conf_id not in buffers.keys()):
+        buffers[conf_id] = {}
+
+    if (check.get('type') == 'connection'): # period start and period end
+        res = conn_search(q=check.get('query'), l=check.get('checks') or 5)
+        rich.print(res)
+
+    if (check.get('display') != None and check.get('display') != False):
+        for sockid in CLIENTS:
+            try:
+                CLIENTS[sockid].send(json.dumps({
+                    'id': conf_id, 
+                    'name': check.get('name') or '',
+                    'show_dir': check.get('show_dir') if check.get('show_dir') != None else True,
+                    'show_track': check.get('show_track') if check.get('show_track') != None else True,
+                    'corder': check.get('display') or 50,
+                    'elems': res
+                }))
+            except websockets.exceptions.ConnectionClosedError:
+                if (sockid in CLIENTS):
+                    CLIENTS.pop(sockid)
+                rich.print(f"[yellow]unsubscribed {str(sockid)[:8]}")
+
+    for elem in res:
+        try:
+            check_changes(elem, check)
+        except Exception as e:
+            rich.print("[red]", 'unexpected error', e, elem)
+
+    tcmp = round(datetime.datetime.timestamp(datetime.datetime.now())) - ((check.get("check_frequency") or 30) * 0.8)
+    buffers[conf_id] = {k: v for k, v in buffers[conf_id].items() if v["last_update"] > tcmp}
+
+
+def updater(check):
+    rich.print(f"[green]updater {check['id']} started")
+    while True:
+        try:
+            update(check)
+        except Exception as e:
+            rich.print(f"[red]Whole loop error: {str(e)}")
+        time.sleep(check.get("check_frequency") or 60)
+
+@click.command()
+@click.option("--server", "-s", type=bool, is_flag=True, default=False, help="update all")
+def main(server=False):
+    threads = []
+
+    for check in conf_manager.check_list:
+        threads.append(threading.Thread(target=updater, args=(check,)))
+        threads[-1].start()
+        time.sleep(1)
+
+    if (server):
+        rich.print("[magenta]Started server")
+    else:
+        rich.print("[magenta]SERVERLESS MODE")
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    main()
